0d8c7e8480ba329432ad5ad8ed846de44ced4397
Peter Griess <pgriess@gmail.com>
2010-02-18 15:01:48 -0600

    Create distutils package.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name = 'PyCascade',
+    version = '1.0',
+    author = 'Peter Griess',
+    author_email = 'pgriess@gmail.com',
+    maintainer = 'Peter Griess',
+    maintainer_email = 'pgriess@gmail.com',
+    url = 'http://github.com/pgriess/PyCascade',
+    description = 'A Python client for Cascade, the Yahoo! Mail API',
+    long_description = 'A Python implementation of a `Cascade <http://developer.yahoo.com/mail/>`_ client; uses `OAuth <http://developer.yahoo.com/oauth/>`_ for authorization.',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Communications :: Email',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    py_modules = ['cascade'],
+    requires = ['oauth'],
+    provides = ['cascade']
+)
